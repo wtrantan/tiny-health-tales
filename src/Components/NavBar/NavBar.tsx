f483d1ac4d95412a04fffc8a54c9f0c09763046c
Pyroniam <angel.pm7913@gmail.com>
2023-12-21 16:04:04 -0800

    Added logo to NavBar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./NavBar.css";
+import logo from './TinyHealthTalesLogo.png'
 
 export default function NavBar() {
   const pages = ["Home", "Courses", "About", "Contact"];
   return (
     <div className="topBar">
-      <h1>Logo Here</h1>
+      <h1><img src={logo} alt="Logo" /></h1>
       <ul>
         {pages.map((page) => (
           <li key={page}>
