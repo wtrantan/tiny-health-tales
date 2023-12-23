f281f4ae48cef80c4b313af66d01bf38ffdc7a99
Psalm Bautista <psalm.bautista13524@gmail.com>
2023-12-22 19:06:21 -0800

    Page Sections Added/Needs Image Styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.tsx";
+import About from "./Pages/About/About.tsx";
+import Courses from "./Pages/Courses/Courses.tsx";
+
 import "./App.css";
 
 export default function App() {
@@ -11,8 +14,8 @@ export default function App() {
         <Route path="*" element={<Home />} />
         <Route index element={<Home />} />
         <Route path="Home" element={<Home />} />
-        <Route path="Courses" element={<></>} />
-        <Route path="About" element={<></>} />
+        <Route path="Courses" element={<Courses />} />
+        <Route path="About" element={<About />} />
         <Route path="Contact" element={<></>} />
       </Routes>
     </BrowserRouter>
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./NavBar.css";
-import logo from './TinyHealthTalesLogo.png'
+import logo from "./TinyHealthTalesLogo.png";
 
 export default function NavBar() {
-  const pages = ["Home", "Courses", "About", "Contact"];
+  const pages = ["Courses", "About", "Contact"];
   return (
     <div className="topBar">
-      <h1><img src={logo} alt="Logo" /></h1>
+      <a href="/Home">
+        <img src={logo} alt="Logo" />
+      </a>
       <ul>
         {pages.map((page) => (
           <li key={page}>
