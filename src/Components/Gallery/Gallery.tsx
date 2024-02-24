013e21f7f2bc71e0ff1aa24b90411eede146005b
wtrantan <wtrantan@gmail.com>
2024-02-23 16:37:01 -0800

    Youtube grid

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Gallery.css";
 import x from "./Flu_Awareness_For_All_FLYER.png";
+import y from "./All_About_Diabetes.pdf";
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
@@ -11,6 +12,7 @@ function MyVerticallyCenteredModal(props) {
       size="xl"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      fullscreen="xl-down"
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
@@ -19,8 +21,8 @@ function MyVerticallyCenteredModal(props) {
       </Modal.Header>
       <Modal.Body>
         <h4>Centered Modal</h4>
-        <img src={x}></img>
-      
+        <iframe src={y} width="100%" height="600px" />
+    
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
--- src/Pages/Videos/Videos.tsx
+++ src/Pages/Videos/Videos.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import NavBar from "../../Components/NavBar/NavBar.tsx";
 import Footer from "../../Components/Footer/Footer.tsx";
 import "./Videos.css";
-
+import YoutubeGallery from "../../Components/VideoItem/YoutubeGallery.tsx";
+import Logo from "./THTLogoF.png";
 export default function Videos() {
   return (
     <>
       <NavBar page="Videos" />
+      <div className="vid-page">
+        <img src={Logo}></img>
+        <h4>Welcome to Tiny Health Tales!</h4>
+
+      </div>
+      <div className="App">
+      <YoutubeGallery />
+    </div>
       <Footer/>
     </>
   );
