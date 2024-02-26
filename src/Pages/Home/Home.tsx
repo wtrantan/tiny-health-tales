70ca546186f9911249e629e4473b25414870d1cb
wtrantan <wtrantan@gmail.com>
2024-02-25 23:58:06 -0800

    removed a video form home page and added event image for news

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,6 +6,8 @@ import Footer from "../../Components/Footer/Footer.tsx";
 import Group1 from "./Group1.jpg";
 import Gallery from "../../Components/Gallery/Gallery.tsx";
 import "../Videos/Videos.css";
+import "../Flyers/Flyers.css";
+import Event from "../../Assets/THT_Event.jpg"
 import flu from "../../Assets/Flyers/Flu_Awareness_For_All_FLYER.png";
 import diabetes from "../../Assets/Flyers/All About Diabetes.png";
 
@@ -14,7 +16,7 @@ export default function Home() {
     { width: "100%", height: "100%", src: 'TnQlUrk2vKM', title: 'Tiny Health Tales: Flu Awareness' },
     { width: "100%", height: "100%", src: '8SMLO9kCAH4', title: 'Tiny Health Tales: Understanding Diabetes' },
     { width: "100%", height: "100%", src: 'fUKM5fX8VZU', title: 'Tiny Health Tales Presents: Keeping our Mouths Clean and Healthy' },
-    { width: "100%", height: "100%", src: 'Q4BGOZNXxkE', title: 'Cleaning and Healing Owies' },
+  
 
   ];
   const flyers = [
@@ -34,9 +36,9 @@ export default function Home() {
             <hr className="Home divider"/>
           </div>   
           <div className="Home block1 image-grid">
-          <img src={Group1} alt="Image 1 Description"/>
-          <img src={Group1} alt="Image 2 Description"/>
-          <img src={Group1} alt="Image 3 Description"/>
+          <img src={Event} alt="Image 1 Description"/>
+          <img src={Event} alt="Image 2 Description"/>
+          <img src={Event} alt="Image 3 Description"/>
           </div>
         </div>
         <div className="Home block2">
@@ -51,8 +53,8 @@ export default function Home() {
         </div>
           </div>   
         </div>
-        <div className="Home block2">
-          <div className="Home title">          
+        <div className="Home block3">
+          <div className="Home title ">          
             <h3>Flyers<button className="Home see-more"><a href="/Flyers">See More</a></button></h3>
             <hr className="Home divider"/>
             <div className="Flyer body">
